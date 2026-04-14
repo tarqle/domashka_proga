6133c633e954859306e11ac85fe1b037610449cb
tarqle <tarqlecs@gmail.com>
2026-04-14 17:03:50 +0500

    00_distance.py

--- 00_distance.py
+++ 00_distance.py
@@ -1,20 +1,23 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# Есть словарь координат городов
-
 sites = {
     'Moscow': (550, 370),
     'London': (510, 510),
     'Paris': (480, 480),
 }
 
-# Составим словарь словарей расстояний между ними
-# расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-
 distances = {}
 
-# TODO здесь заполнение словаря
+# Заполнение словаря
+for city1, coords1 in sites.items():
+    distances[city1] = {}
+    for city2, coords2 in sites.items():
+        if city1 != city2:
+            # Вычисляем расстояние между городами
+            x1, y1 = coords1
+            x2, y2 = coords2
+            distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+            distances[city1][city2] = round(distance, 2)  # Округляем до 2 знаков
+        else:
+            distances[city1][city2] = 0
 
 print(distances)
 
